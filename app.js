8902d9da74ab8dbaa5118e073c6de529b764d21d
LukvonStrom <lukas@fruntke.tech>
2018-04-19 17:55:32 +0200

    :recycle: Change App init cycle

--- app.js
+++ app.js
@@ -7,46 +7,42 @@ const cors = require('cors');
 const RenewController = require('./controller/authentication/RenewController');
 const Raven = require('raven');
 
-require('dotenv').load();
 
 const app = express();
-
-require('./mongoose').connect().then(() => {
-  const { middleware } = require('./middleware/AuthChecker');
-  const LoginController = require('./controller/authentication/LoginController');
-  const Strategy = require('./controller/authentication/passport/login');
+const { middleware } = require('./middleware/AuthChecker');
+const LoginController = require('./controller/authentication/LoginController');
+const Strategy = require('./controller/authentication/passport/login');
 
 
-  Raven.config(process.env.DSN).install();
-  app.use(Raven.requestHandler());
-  app.use(Raven.errorHandler());
-  app.use((err, req, res, next) => {
-    res.statusCode = 500;
-    res.end(`${res.sentry}\n`);
-  });
+Raven.config(process.env.DSN).install();
+app.use(Raven.requestHandler());
+app.use(Raven.errorHandler());
+app.use((err, req, res, next) => {
+  res.statusCode = 500;
+  res.end(`${res.sentry}\n`);
+});
 
 
-  app.use(cors());
+app.use(cors());
 
-  app.use(express.static(path.join(__dirname, './public/build')));
-  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/build', 'index.html')))
+app.use(express.static(path.join(__dirname, './public/build')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/build', 'index.html')));
 
-  app.use(bodyParser.json());
-  app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use(passport.initialize());
 
-  passport.use('local-login', Strategy);
+passport.use('local-login', Strategy);
 
-  app.use('/api', middleware);
-  app.post('/api/auth', LoginController);
-  app.post('/api/auth/renew', RenewController);
-  app.post('/api/substitution', controller);
+app.use('/api', middleware);
+app.post('/api/auth', LoginController);
+app.post('/api/auth/renew', RenewController);
+app.post('/api/substitution', controller);
 
-  app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    res.status(err.status || 500);
-    res.json(err);
-  });
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.json(err);
 });
 
 module.exports = app;
--- bin/www.js
+++ bin/www.js
@@ -1,55 +1,53 @@
 #!/usr/bin/env node
-
-const normalizePort =require("../util/normalizePort");
-const app = require('../app');
+const normalizePort = require('../util/normalizePort');
 const debug = require('debug')('absence:server');
 const http = require('http');
+require('dotenv').load();
 
-let port = normalizePort(process.env.PORT || '8080');
-app.set('port', port);
+let server;
 
-const server = http.createServer(app);
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Event listener for HTTP server "error" event.
- */
+const port = normalizePort(process.env.PORT || '8080');
 
 function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-    let bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
+  const bind = typeof port === 'string'
+    ? `Pipe ${port}`
+    : `Port ${port}`;
 
     // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
-
-/**
- * Event listener for HTTP server "listening" event.
- */
 
 function onListening() {
-    let addr = server.address();
-    let bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    debug('Listening on ' + bind);
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? `pipe ${addr}`
+    : `port ${addr.port}`;
+  console.log(`Listening on ${bind}`);
 }
+
+require('../mongoose').connect().then(() => {
+  const app = require('../app');
+
+  app.set('port', port);
+
+  server = http.createServer(app);
+
+  server.listen(port);
+  server.on('error', onError);
+  server.on('listening', onListening);
+});
