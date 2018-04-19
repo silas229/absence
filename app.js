9ea9743789e9938c6fa0dfe9d9af8a28a07d39bb
LukvonStrom <lukas@fruntke.tech>
2018-04-19 17:10:45 +0200

    :sparkles: Adding React compatible Routing

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const path = require('path');
 const controller = require('./controller/substitution/controller');
 const cors = require('cors');
@@ -12,45 +12,42 @@ require('dotenv').load();
 const app = express();
 
 require('./mongoose').connect().then(() => {
-    const middleware = require("./middleware/AuthChecker").middleware;
-    const LoginController = require("./controller/authentication/LoginController");
-    const Strategy  = require("./controller/authentication/passport/login");
+  const { middleware } = require('./middleware/AuthChecker');
+  const LoginController = require('./controller/authentication/LoginController');
+  const Strategy = require('./controller/authentication/passport/login');
 
 
-    Raven.config(process.env.DSN).install();
-    app.use(Raven.requestHandler());
-    app.use(Raven.errorHandler());
-    app.use(function onError(err, req, res, next) {
-        res.statusCode = 500;
-        res.end(res.sentry + '\n');
-    });
+  Raven.config(process.env.DSN).install();
+  app.use(Raven.requestHandler());
+  app.use(Raven.errorHandler());
+  app.use((err, req, res, next) => {
+    res.statusCode = 500;
+    res.end(`${res.sentry}\n`);
+  });
 
 
-    app.use(cors());
+  app.use(cors());
 
-    app.use(express.static(path.join(__dirname, '/../client/build')));
+  app.use(express.static(path.join(__dirname, './public/build')));
+  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, './public/build', 'index.html')))
 
-    app.use(bodyParser.json());
-    app.use(passport.initialize());
+  app.use(bodyParser.json());
+  app.use(passport.initialize());
 
-    passport.use('local-login', Strategy);
+  passport.use('local-login', Strategy);
 
-    app.use('/api', middleware);
+  app.use('/api', middleware);
+  app.post('/api/auth', LoginController);
+  app.post('/api/auth/renew', RenewController);
+  app.post('/api/substitution', controller);
 
-    app.post('/auth', LoginController);
-
-    app.post('/auth/renew', RenewController)
-
-    app.post('/api/substitution', controller);
-
-    app.use(function(err, req, res, next) {
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-        res.status(err.status || 500);
-        res.json(err);
-    });
+  app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.json(err);
+  });
 });
 
 module.exports = app;
-
 
