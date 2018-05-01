7ca998181cdfeba5e960c3235ff02ca3c46bc689
LukvonStrom <lukas@fruntke.tech>
2018-05-01 22:17:25 +0200

    :recycle: Refactor for Lint Swag.

--- controller/authentication/LoginController.js
+++ controller/authentication/LoginController.js
@@ -1,36 +1,36 @@
 const passport = require('passport');
 
 module.exports = async (req, res, next) => {
-    // Plug in Validation :)
+  // Plug in Validation :)
 
-    let validation = true;
-    if(!validation){
-        return res.status(400).json({
-            success: false,
-            //message: validationResult.message,
-            //errors: validationResult.errors,
-        })
+  const validation = true;
+  if (!validation) {
+    return res.status(400).json({
+      success: false,
+      // message: validationResult.message,
+      // errors: validationResult.errors,
+    });
+  }
+
+  return passport.authenticate('local-login', (err, token, userData) => {
+    if (err || token === false || userData.message === 'Missing credentials') {
+      let error = 'InternalError';
+      if (err.name === 'IncorrectCredentialsError') {
+        error = err.name;
+      }
+      console.error(err);
+      return res.status(400).json({
+        success: false,
+        message: error,
+      });
     }
 
-    return passport.authenticate('local-login', (err, token, userData) => {
-        if(err || token === false || userData.message === "Missing credentials") {
-            let error = "InternalError";
-            if(err.name === 'IncorrectCredentialsError'){
-                error = err.name;
-            }
-            console.error(err);
-            return res.status(400).json({
-                success: false,
-                message: error
-            });
-        }
-
-        return res.status(200).json({
-            success: true,
-            message: 'Logged in!',
-            token,
-            user: userData,
-        });
-    })(req, res, next);
+    return res.status(200).json({
+      success: true,
+      message: 'Logged in!',
+      token,
+      user: userData,
+    });
+  })(req, res, next);
 };
 
--- controller/authentication/passport/login.js
+++ controller/authentication/passport/login.js
@@ -1,45 +1,44 @@
+/* eslint-disable consistent-return,no-underscore-dangle */
 const jwt = require('jsonwebtoken');
-const User = require('mongoose').model('User');
 const PassportLocal = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 
-module.exports= new PassportLocal({
-    usernameField: 'email',
-    passwordField: 'password',
-    session: false,
-    passReqToCallback: true
+const User = global.mongoose.model('User');
+
+function wrong() {
+  const error = new Error('Incorrect Credentials');
+  error.name = 'IncorrectCredentialsError';
+  return error;
+}
+
+module.exports = new PassportLocal({
+  usernameField: 'email',
+  passwordField: 'password',
+  session: false,
+  passReqToCallback: true,
 }, async (req, email, password, done) => {
-    const userData = {
-        email: email.trim(),
-        password: password.trim(),
-    };
+  const userData = {
+    email: email.trim(),
+    password: password.trim(),
+  };
 
-    try{
-        let user = await User.findOne({email: userData.email});
+  try {
+    const user = await User.findOne({ email: userData.email });
 
-        if(!user){
-            return done(wrong());
-        }
-
-        let requesting_pw = userData.password;
-        let pw = user.password;
-        if(await bcrypt.compare(requesting_pw, pw)) {
-                const token = jwt.sign({sub:user._id}, process.env.JWT_SECRET, {expiresIn: '168h'});
-                return done(null, token, {name: user.name, grade: user.grade});
-        }
-
-        return done(wrong());
-
-
-    } catch(e){
-        done(e);
+    if (!user) {
+      return done(wrong());
     }
 
-});
+    const passwordAttempt = userData.password;
+    const pw = user.password;
+    if (await bcrypt.compare(passwordAttempt, pw)) {
+      const token = jwt.sign({ sub: user._id }, process.env.JWT_SECRET, { expiresIn: '168h' });
+      return done(null, token, { name: user.name, grade: user.grade });
+    }
 
-function wrong(){
-    const error = new Error('Incorrect Credentials');
-    error.name = 'IncorrectCredentialsError';
-    return error;
-}
+    return done(wrong());
+  } catch (e) {
+    done(e);
+  }
+});
 
